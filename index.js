b3f878eccd6ac0f80562514b8b52a891acffd82d
sushmita-singh <sushmitas326@gmail.com>
2022-08-12 14:30:21 +0530

    fix: added an end screen

--- index.js
+++ index.js
@@ -2,15 +2,33 @@ import dogs from "./data.js";
 import {Dog} from "./Dog.js";
 
 const dogArray = ["rex", "bella", "teddy"]
+const likedArray = []
 
 let currentDog = getNextDog()
-const badgeLikeEl = document.getElementById('badge-like')
-const badgeNopeEl = document.getElementById('badge-nope')
+// const badgeLikeEl = document.getElementById('badge-like')
+// const badgeNopeEl = document.getElementById('badge-nope')
 const likeBtn = document.getElementById('btn-like')
 const dislikeBtn = document.getElementById('btn-dislike')
+const tryAgainBtn = document.getElementById('try-again')
 
-likeBtn.addEventListener('click', currentDog.setLikeBadge)
-dislikeBtn.addEventListener('click', setDislikeBadge)
+likeBtn.addEventListener('click', function() {
+    currentDog.setLikeBadge()
+    likedArray.push(currentDog)
+    loadNextDog()
+})
+dislikeBtn.addEventListener('click', function() {
+    currentDog.setDislikeBadge()
+    loadNextDog()
+})
+tryAgainBtn.addEventListener('click', reset)
+
+function reset() {
+    console.log("button clicked")
+    dogArray = ["rex", "bella", "teddy"]
+    likedArray = []
+    currentDog = getNextDog()
+    render()
+}
 
 function render() {
     document.getElementById('main').innerHTML = currentDog.setDogHtml()
@@ -25,16 +43,55 @@ function getNextDog() {
     }
 }
 
-function setDislikeBadge() {
-    currentDog.hasBeenLiked = true
-    currentDog.hasbeenSwiped = true
-    currentDog.document.getElementById('badge-nope').classList.add('hidden')        
-    currentDog.document.getElementById('badge-nope').classList.remove('show')
-    currentDog.document.getElementById('badge-like').classList.remove('hidden')
-    currentDog.document.getElementById('badge-like').classList.add('show')
+function loadNextDog() {
+    disableButtons()
+    setTimeout(()=>{
+        if( dogArray.length > 0) {
+            currentDog = getNextDog()
+            enableButtons()
+            render()
+        } else {
+            showResult()
+        }
+    },500)
 }
 
-function setLikeBadge() {
-
+function disableButtons() {
+    likeBtn.classList.remove('show')
+    dislikeBtn.classList.remove('show')
+    likeBtn.classList.add('hidden')
+    dislikeBtn.classList.add('hidden')
 }
+
+function enableButtons() {
+    likeBtn.classList.remove('hidden')
+    dislikeBtn.classList.remove('hidden')
+    likeBtn.classList.add('show')
+    dislikeBtn.classList.add('show')
+}
+
+function showResult() {
+        if(likedArray.length > 0) {
+            let index = Math.floor(Math.random() * likedArray.length)
+            console.log(index)
+            console.log(likedArray[index])
+            document.getElementById('main').innerHTML = `<div class="result">
+                    <h1>You are matched with <span class = "ascent">${likedArray[index].name}</span></h1>
+                    <div class = "show-match">
+                        <img class="match self" src="images/dog-profile.jpg">                
+                        <img class="match partner" src="${likedArray[index].avatar}">
+                    <div>
+                </div>
+                `
+        } else {
+            document.getElementById('main').innerHTML = `<div class="result">
+                    <h1>You are not matched with anyone</h1>
+                </div>
+                `
+                
+                // <img src="images/try-again.jpg" id = "try-again">
+                    // <button id="try-again" class="show">Try Again!</button>
+        }
+}
+
 render()
